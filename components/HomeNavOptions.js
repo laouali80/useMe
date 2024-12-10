3cbabb130f1bb55d0ebb44dcfb765dac2bab5ad9
Laouali <laoualibachir2000@gmail.com>
2024-12-10 03:37:06 +0100

    render the wallet and select nav options

--- components/HomeNavOptions.js
+++ components/HomeNavOptions.js
@@ -7,40 +7,45 @@ import {
   View,
 } from "react-native";
 import React from "react";
+import tw from "twrnc";
+import shop from "../assets/images/shop.png";
 
 const data = [
   {
     id: 1,
     title: "Get a ride",
-    image: "",
+    image: require("../assets/images/taxi-cab.png"),
     screen: "MapScreen",
   },
   {
     id: 2,
     title: "Shop Now",
-    image: "",
+    image: require("../assets/images/shop.png"),
     screen: "ShopScreen",
   },
 ];
 
 const HomeNavOptions = () => {
   return (
-    <FlatList
-      data={data}
-      horizontal
-      keyExtractor={(item) => item.id}
-      renderItem={({ item }) => {
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-          <View>
-            <Image
-              style={{ width: 120, height: 120, resizeMode: "contain" }}
-              source={{}}
-            />
-            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-          </View>
-        </TouchableOpacity>;
-      }}
-    />
+    <View>
+      <Text>Select !</Text>
+      <FlatList
+        data={data}
+        horizontal
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-white m-2 w-40`}>
+            <View>
+              <Image
+                style={{ width: 120, height: 120, resizeMode: "contain" }}
+                source={item.image}
+              />
+              <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
   );
 };
 
--- components/HomeWallet.js
+++ components/HomeWallet.js
@@ -1,10 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import tw from "twrnc";
 
 const HomeWallet = () => {
   return (
-    <View>
-      <Text style={{}}>HomeWallet</Text>
+    <View style={tw`bg-[#c8d6fd] p-5 my-5`}>
+      <Text style={{}}>Wallet Balance</Text>
+      <Text style={tw`font-semibold text-2xl`}>N 10,500</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <View>
+          <Text>Account Number:</Text>
+          <Text style={tw`text-[#5c89f4]`}>1234567890</Text>
+        </View>
+        <View>
+          <Pressable style={tw`bg-[#2358ea] p-1`}>
+            <Text>+ Fund wallet</Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 };
