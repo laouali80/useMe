7a8bbd2594bfaee46cefcae1225b6f360fcab8a6
Laouali <laoualibachir2000@gmail.com>
2024-12-10 05:06:48 +0100

    render the react native map

--- App.js
+++ App.js
@@ -3,9 +3,16 @@ import { StyleSheet, Text, View } from "react-native";
 // import Login from "./screens/authScreens/LoginScreen";
 import Signup from "./screens/authScreens/SignupScreen";
 import HomeScreen from "./screens/homeScreen/HomeScreen";
+import MapScreen from "./screens//rideScreens/MapScreen";
+
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <NavigationContainer>
+      <MapScreen />
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
