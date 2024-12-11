b4568c838fbcabb0b20f5ed08a73d92bf72c543a
Laouali <laoualibachir2000@gmail.com>
2024-12-11 01:58:47 +0100

    ajust the style

--- screens/authScreens/LoginScreen.js
+++ screens/authScreens/LoginScreen.js
@@ -3,41 +3,43 @@ import React from "react";
 
 const loginScreen = () => {
   return (
-    <View>
-      <Text>UseMe.</Text>
-      <Text>Create your UseMe account.</Text>
-      <Text>Fill your details below to login into your UseMe account.</Text>
+    <SafeAreaView style={tw`bg-white h-full w-full`}>
+      <View style={tw`p-5`}>
+        <Text style={tw`text-[]`}>UseMe.</Text>
+        <Text>Create your UseMe account.</Text>
+        <Text>Fill your details below to login into your UseMe account.</Text>
 
-      {/* PHONE NUMBER */}
-      <Text>Phone Number</Text>
-      <TextInput
-        //   inlineImageLeft={}
-        placeholder="Enter your phone number"
-      />
+        {/* PHONE NUMBER */}
+        <Text>Phone Number</Text>
+        <TextInput
+          //   inlineImageLeft={}
+          placeholder="Enter your phone number"
+        />
 
-      {/* PIN */}
-      <Text>Pin</Text>
-      <TextInput
-        //   inlineImageLeft={}
-        placeholder="Enter pin again"
-        secureTextEntry={true}
-        autoCapitalize="none"
-      />
-      <Text>Forget Pin?</Text>
+        {/* PIN */}
+        <Text>Pin</Text>
+        <TextInput
+          //   inlineImageLeft={}
+          placeholder="Enter pin again"
+          secureTextEntry={true}
+          autoCapitalize="none"
+        />
+        <Text>Forget Pin?</Text>
 
-      <Button
-        title="Create Account"
-        style={styles.button}
-        onPress={loginValidation}
-      />
+        <Button
+          title="Create Account"
+          style={styles.button}
+          onPress={loginValidation}
+        />
 
-      <Text>
-        Don't have an account?
-        <Pressable onPress={{}} style={{}}>
-          Create Account
-        </Pressable>
-      </Text>
-    </View>
+        <Text>
+          Don't have an account?
+          <Pressable onPress={{}}>
+            <Text style={tw`text-[]`}>Create Account</Text>
+          </Pressable>
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- screens/authScreens/SignupScreen.js
+++ screens/authScreens/SignupScreen.js
@@ -3,62 +3,64 @@ import React from "react";
 
 const SignupScreen = () => {
   return (
-    <View>
-      <Text>UseMe.</Text>
-      <Text>Create your UseMe account.</Text>
+    <SafeAreaView style={tw`bg-white h-full w-full`}>
+      <View style={tw`p-5`}>
+        <Text style={tw`text-[]`}>UseMe.</Text>
+        <Text>Create your UseMe account.</Text>
 
-      {/* FIRST NAME */}
-      <Text>First Name</Text>
-      <TextInput
-        //   inlineImageLeft={}
-        placeholder="Enter your first name"
-      />
+        {/* FIRST NAME */}
+        <Text>First Name</Text>
+        <TextInput
+          //   inlineImageLeft={}
+          placeholder="Enter your first name"
+        />
 
-      {/* LAST NAME */}
-      <Text>Last Name</Text>
-      <TextInput
-        //   inlineImageLeft={}
-        placeholder="Enter your last name"
-      />
+        {/* LAST NAME */}
+        <Text>Last Name</Text>
+        <TextInput
+          //   inlineImageLeft={}
+          placeholder="Enter your last name"
+        />
 
-      {/* PHONE NUMBER */}
-      <Text>Phone Number</Text>
-      <TextInput
-        //   inlineImageLeft={}
-        placeholder="Enter your phone number"
-      />
+        {/* PHONE NUMBER */}
+        <Text>Phone Number</Text>
+        <TextInput
+          //   inlineImageLeft={}
+          placeholder="Enter your phone number"
+        />
 
-      {/* CREATE PIN */}
-      <Text>Create Pin</Text>
-      <TextInput
-        //   inlineImageLeft={}
-        placeholder="Enter your preferred pin"
-        secureTextEntry={true}
-        autoCapitalize="none"
-      />
+        {/* CREATE PIN */}
+        <Text>Create Pin</Text>
+        <TextInput
+          //   inlineImageLeft={}
+          placeholder="Enter your preferred pin"
+          secureTextEntry={true}
+          autoCapitalize="none"
+        />
 
-      {/* CONFIRM PIN */}
-      <Text>Confirm Pin</Text>
-      <TextInput
-        //   inlineImageLeft={}
-        placeholder="Enter pin again"
-        secureTextEntry={true}
-        autoCapitalize="none"
-      />
+        {/* CONFIRM PIN */}
+        <Text>Confirm Pin</Text>
+        <TextInput
+          //   inlineImageLeft={}
+          placeholder="Enter pin again"
+          secureTextEntry={true}
+          autoCapitalize="none"
+        />
 
-      <Button
-        title="Create Account"
-        style={styles.button}
-        onPress={loginValidation}
-      />
+        <Button
+          title="Create Account"
+          style={styles.button}
+          onPress={loginValidation}
+        />
 
-      <Text>
-        Already have an account?
-        <Pressable onPress={{}} style={{}}>
-          Sign In
-        </Pressable>
-      </Text>
-    </View>
+        <Text>
+          Already have an account?
+          <Pressable onPress={{}} style={{}}>
+            Sign In
+          </Pressable>
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
