7ef6db1f44860c90060b6629aadcddd5c4310ade
Laouali <laoualibachir2000@gmail.com>
2024-12-11 02:20:05 +0100

    added the return back to homescreen on map

--- screens/rideScreens/MapScreen.js
+++ screens/rideScreens/MapScreen.js
@@ -6,11 +6,21 @@ import { createStackNavigator } from "@react-navigation/stack";
 import RideOptionsCard from "../../components/ride/RideOptionsCard";
 import DestinationSelection from "../../components/ride/DestinationSelection";
 import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const MapScreen = () => {
   const Stack = createStackNavigator();
+  const navigation = useNavigation();
+
   return (
     <View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("HomeScreen")}
+        style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
+
       <View style={tw`h-1/2`}>
         <Map />
       </View>
