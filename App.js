4519827780ae772faa4c97c98210d155f059e634
Laouali <laoualibachir2000@gmail.com>
2024-12-14 01:30:54 +0100

    started the navigation set up

--- App.js
+++ App.js
@@ -15,21 +15,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import Login from "./screens/authScreens/LoginScreen";
 // import MapScreen from "./screens/rideScreens/mapScreen";
 import Onboarding from "./screens/onboardingScreens/Onboarding";
+import mainNavigation from "./navigation/mainNavigation";
 
 export default function App() {
+  const AppNavigator = mainNavigation();
   return (
-    <NavigationContainer>
+    <SafeAreaProvider>
       {/* this SafeAreaProvider is to protect the icons from danger zone (react elements) */}
-      <SafeAreaProvider>
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-          style={{ flex: 1 }}
-          keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
-        >
-          <Onboarding />
-        </KeyboardAvoidingView>
-      </SafeAreaProvider>
-    </NavigationContainer>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+        keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+      >
+        <AppNavigator />
+      </KeyboardAvoidingView>
+    </SafeAreaProvider>
   );
 }
 
--- components/HomeNavOptions.js
+++ components/HomeNavOptions.js
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import React from "react";
 import tw from "twrnc";
+import { useNavigation } from "@react-navigation/native";
 import shop from "../assets/images/shop.png";
 
 const data = [
@@ -15,7 +16,7 @@ const data = [
     id: 1,
     title: "Get a ride",
     image: require("../assets/images/taxi-cab.png"),
-    screen: "MapScreen",
+    screen: "RideScreen",
   },
   {
     id: 2,
@@ -26,6 +27,15 @@ const data = [
 ];
 
 const HomeNavOptions = () => {
+  const navigation = useNavigation();
+
+  const _navigate = (screen) => {
+    console.log(navigation);
+    if (screen === "RideScreen") {
+      navigation.navigate("Main");
+    }
+  };
+
   return (
     <View>
       <Text>Select !</Text>
@@ -34,7 +44,10 @@ const HomeNavOptions = () => {
         horizontal
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-white m-2 w-40`}>
+          <TouchableOpacity
+            onPress={() => _navigate(item.screen)}
+            style={tw`p-2 pl-6 pb-8 pt-4 bg-white m-2 w-40`}
+          >
             <View>
               <Image
                 style={{ width: 120, height: 120, resizeMode: "contain" }}
